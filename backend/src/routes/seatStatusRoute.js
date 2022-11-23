29092a4662ed024ba7cb73977dc4ec1dba20c550
Bappy60 <kawsar.ahmed.bappy.148@gmail.com>
2022-11-24 01:06:42 +0600

    modified seatStatusRoute

--- backend/src/routes/seatStatusRoute.js
+++ backend/src/routes/seatStatusRoute.js
@@ -3,5 +3,4 @@ const verification = require("../middleware/tokenValidation")
 const seatStatusController = require("../controllers/seatStatusController")
 const router = express.Router();
 router.patch("/update",verification.verifyToken, seatStatusController.updateSeatStatus);
->>>>>>> f303a40f3d10ae9710c9772bb8548425df12ada9
 module.exports = router
