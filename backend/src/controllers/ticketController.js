551d3b949a0e012341dd0a8fe2baa1241e2bf82e
Bappy60 <kawsar.ahmed.bappy.148@gmail.com>
2022-11-24 01:58:22 +0600

    github cdi

--- backend/src/controllers/ticketController.js
+++ backend/src/controllers/ticketController.js
@@ -33,3 +33,36 @@ database.query(query1,[user.ID],(err,results)=>{
     
 })
 }
+//ridwan
+
+exports.showTicket = async(req,res)=>{
+    const {passenger_id,trip_id} = req.query
+    const user = req.user
+    let query1 = "SELECT * FROM `passenger` WHERE passenger_id = ?"
+    
+    database.query(query1,[user.ID],(err,results)=>{
+        
+        if(results && results[0].passenger_id === user.ID ){
+           
+            let query2 = "SELECT * FROM ticket,passenger,trip,train WHERE   train.train_id = trip.train_id AND ticket.passenger_id= passenger.passenger_id AND trip.trip_id = ? and passenger.passenger_id = ? ;"
+            database.query(query2,[trip_id,passenger_id],(err,results)=>{
+                  
+                if(!err){
+                    
+                    res.status(200).json(results)
+                }
+                else{
+                    
+                    res.status(500).err
+                }
+    
+            })
+    
+            
+        }
+        else{
+            res.status(401).json("not permitted")
+        }
+        
+    })
+    }
